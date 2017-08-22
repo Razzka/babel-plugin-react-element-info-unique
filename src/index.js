fa5ee5eeefd1724eeea8e73f67972a6a9053f826
Aleksandr Ketov <aleksandr.ketov@csssr.com>
2017-08-22 16:46:50 +0500

    Add unique ids for identical components.

--- src/index.js
+++ src/index.js
@@ -1,47 +1,55 @@
 // @flow weak
 
-export default function({types: t}) {
-  const defaultPrefix = 'data-qa';
-  let prefix;
-  let filenameAttr;
-  let nodeNameAttr;
+export default function babelPluginReactElementInfo({ types: t }) {
+    const defaultPrefix = 'data-qa';
+    let prefix;
+    let filenameAttr;
+    let nodeNameAttr;
+    let fileNames = {};
 
-  const visitor = {
-    Program(path, state) {
-      if (state.opts.prefix) {
-        prefix = `data-${state.opts.prefix}`;
-      } else {
-        prefix = defaultPrefix;
-      }
-      filenameAttr = `${prefix}-file`;
-      nodeNameAttr = `${prefix}-node`;
-    },
-    JSXOpeningElement(path, state) {
-      const attributes = path.container.openingElement.attributes;
+    const visitor = {
+        Program(path, state) {
+            if (state.opts.prefix) {
+                prefix = `data-${state.opts.prefix}`;
+            } else {
+                prefix = defaultPrefix;
+            }
+            filenameAttr = `${prefix}-file`;
+            nodeNameAttr = `${prefix}-node`;
+            fileNames = {};
+        },
+        JSXOpeningElement(path, state) {
+            const attributes = path.container.openingElement.attributes;
 
-      const newAttributes = [];
+            const newAttributes = [];
 
-      if (path.container && path.container.openingElement &&
-        path.container.openingElement.name &&
-        path.container.openingElement.name.name) {
-        newAttributes.push(t.jSXAttribute(
-          t.jSXIdentifier(nodeNameAttr),
-          t.stringLiteral(path.container.openingElement.name.name))
-        );
-      }
+            if (path.container && path.container.openingElement
+                && path.container.openingElement.name
+                && path.container.openingElement.name.name) {
+                newAttributes.push(t.jSXAttribute(
+                    t.jSXIdentifier(nodeNameAttr),
+                    t.stringLiteral(path.container.openingElement.name.name))
+                );
+            }
 
-      if (state.file && state.file.opts && state.file.opts.basename) {
-        newAttributes.push(t.jSXAttribute(
-          t.jSXIdentifier(filenameAttr),
-          t.stringLiteral(state.file.opts.basename))
-        );
-      }
+            if (state.file && state.file.opts && state.file.opts.basename) {
+                const name = state.file.opts.basename;
+                let count = fileNames[name] || 1;
+                const nameIdx = `${name}${count}`;
 
-      attributes.push(...newAttributes);
-    },
-  };
+                fileNames[name] = count + 1;
 
-  return {
-    visitor,
-  };
+                newAttributes.push(t.jSXAttribute(
+                    t.jSXIdentifier(filenameAttr),
+                    t.stringLiteral(nameIdx))
+                );
+            }
+
+            attributes.push(...newAttributes);
+        }
+    };
+
+    return {
+        visitor
+    };
 }
